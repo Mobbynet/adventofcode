061965e7a7d8dce5bf7d77ca75e2c3b323c84034
Michal Grzesik <275465@student.pwr.edu.pl>
2022-12-22 20:31:49 +0100

    Created struct List and proabally loaded input wrong but its going to be show in the next algorthim.

---
+++ Puzzle13/File_loading.c
@@ -0,0 +1,5 @@
+//
+// Created by michal on 19.12.22.
+//
+
+#include "File_loading.h"
---
+++ Puzzle13/File_loading.h
@@ -0,0 +1,8 @@
+//
+// Created by michal on 19.12.22.
+//
+
+#ifndef PUZZLE13_FILE_LOADING_H
+#define PUZZLE13_FILE_LOADING_H
+
+#endif //PUZZLE13_FILE_LOADING_H
